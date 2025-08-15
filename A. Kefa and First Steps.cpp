17cb4530aad8a0174e612cb3adfc334a86219f27
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-15 23:41:49 +0530

    A. Kefa and First Steps.cpp

---
+++ A. Kefa and First Steps.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+    
+    int max_len = 1;
+    int current_len = 1;
+    
+    for (int i = 1; i < n; ++i) {
+        if (a[i] >= a[i-1]) {
+            current_len++;
+            max_len = max(max_len, current_len);
+        } else {
+            current_len = 1;
+        }
+    }
+    
+    cout << max_len << endl;
+    return 0;
+}
